1bfe1ee2126bd3167a941d2589c799fbe6c7764a
Thiago Alexandre <thiagoalex.desouza@gmail.com>
2019-05-18 18:26:15 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -11,11 +11,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(P);
-
-            Console.ReadLine();
+            Tela.imprimirTabuleiro(tab);
 
         }
     }
--- xadrez_console/Tabuleiro/Tabuleiro.cs
+++ xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tabuleiro
+namespace tabuleiro
 {
     class Tabuleiro
     {
@@ -17,6 +17,11 @@ namespace Tabuleiro
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
